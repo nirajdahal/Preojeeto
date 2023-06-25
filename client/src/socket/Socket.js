b2fea54502b4cd365535bb6b67c83ca3b890eede
DESKTOP-7LN1193\niraj <nirajdahal09@gmail.com>
2023-06-26 03:34:54 +1000

    socket-io modification

--- client/src/socket/Socket.js
+++ client/src/socket/Socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 import { getNotifications } from "../features/common/slice/NotificationSlice";
 let socketInstance;
 export const initializeSocket = (token, user) => {
-    socketInstance = io('https://projeeto.onrender.com', {
+    socketInstance = io('https://projeeto.onrender.com/', {
         query: { token },
     });
     socketInstance.emit("new-user-add", user._id)
